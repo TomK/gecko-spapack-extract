31d6de4d229b177656d96cb2fec46b3c56232a0d
Tom Kay <oridan82@gmail.com>
2021-07-28 20:04:52 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Text;
+using K4os.Compression.LZ4;
+
+namespace intouch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dllPath = Path.GetFullPath(@"./apk/unknown/assemblies/InTouchLib_Generic.dll");
+
+            // xamarin decompress dll (if required)
+            byte[] bytes;
+            byte[] head = new byte[12];
+            var file = File.OpenRead(dllPath);
+            file.Read(head, 0, 12);
+            if (System.Text.Encoding.UTF8.GetString(head[0..4]) == "XALZ")
+            {
+                // determine size
+                uint size = BitConverter.ToUInt32(head[8..12]);
+                // reinit
+                bytes = new byte[size];
+                Console.WriteLine("Decompressing...");
+                using (MemoryStream reader = new MemoryStream())
+                {
+                    file.CopyTo(reader);
+                    LZ4Codec.Decode(reader.ToArray(), bytes);
+                }
+            }
+            else
+            {
+                // dll seems to already be decompressed
+                using (MemoryStream reader = new MemoryStream())
+                {
+                    reader.Write(head);
+                    file.CopyTo(reader);
+                    bytes = reader.ToArray();
+                }
+            }
+
+            Assembly assembly = Assembly.Load(bytes);
+            var resstm = assembly.GetManifestResourceStream("InTouchLib.Resources.SpaPackStruct.xml");
+            var br = new BinaryReader(resstm);
+            var d1 = br.ReadBytes((int)resstm.Length);
+
+            var intouchdata = assembly.CreateInstance("InTouchLib.Util.InTouchData");
+            byte[] d2 = (byte[])intouchdata.GetType().InvokeMember("Read", BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.Public, null, intouchdata, new Object[] { d1 });
+
+            var xml = Encoding.GetEncoding("ISO-8859-1").GetString(d2);
+            File.WriteAllText("SpaPackStruct.xml", xml.TrimEnd('\0'));
+
+            Console.WriteLine("Saved as SpaPackStruct.xml");
+        }
+    }
+}
